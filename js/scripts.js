26adebf0f92abac2cbe3abab9747c556b81557fe
Shirley Keter <shirleyketer@gmail.com>
2019-03-01 17:08:20 +0300

    html js changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
-    $("form#players").submit(function(event) {
-        event.preventDefault();
-    }
+var player1="";
+var player2="";
+
+var diceRoll = function () {
+  return Math.floor(6*Math.random())+1;
+}
+
+function Player(turn) {
+  this.roll = 0;
+  this.roundtotal = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+}
+
+
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.tempscore = 0;
+  alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
+  // this.changeturn();
+  } else {
+  this.tempscore += this.roll;
+  }
+}
