01abc5e01eef0dcf1a76e54dd285849d4be18cf2
Shirley Keter <shirleyketer@gmail.com>
2019-03-04 15:28:03 +0300

    html changes js constructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,30 @@
 $(document).ready(function(){
  $("form#playerName").submit(function(event){
    event.preventDefault();
-   var player1Name = $("input#player1").val();
-   var player2Name = $("input#player2").val();
-    $("#player1Name").text(player1Name)
-    $("#player1Name").text(player1Name)
-  });
-
-var player1="";
-var player2="";
-
-var diceRoll = function () {
-  return Math.floor(6*Math.random())+1;
-}
-
-function playerName(turn) {
-  this.diceroll = 0;
-  this.turnscore = 0;
-  this.totalscore = 0;
-  this.turn = turn;
-  this.player;
-}
-   
-Player.prototype.rollone = function() {
-  if (this.diceroll === 1) {
-  this.turnscore = 0;
-  alert("Sorry " + this.playerName + ", you rolled a 1!")
-  } else {
-  this.turnscore += this.diceroll;
-  }
-}
-Player.prototype.hold = function () {  
-    this.totalscore += this.roundtotal;
+   var player1Name = $("input#Player1").val();
+   var player2Name = $("input#Player2").val();
+    $("#player1Name").text(player1Name);
+    $("#player2Name").text(player2Name);
+  })
+  
+  function player(turn) {
+    this.diceroll = 0;
     this.turnscore = 0;
-    alert(this.playerName + ", your turn is over!");
-}
+    this.totalscore = 0;
+    this.turn = turn;
+    this.playerName;
+  }
+
+  $(".diceroll").click(function player1() {
+    var diceroll = Math.floor(Math.random() * 6) + 1;
+    parseInt($(".totalscore").text(diceRoll));
+  }
+
+
+  
+
+
+
+
     
   
